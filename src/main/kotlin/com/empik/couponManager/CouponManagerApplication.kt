661afa053abc2a84568223efffa2595769b34c6b
bwegrzanowski <bartosz.wegrzanowski@gmail.com>
2026-05-10 15:19:07 +0200

    init

---
+++ src/main/kotlin/com/empik/couponManager/CouponManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.empik.couponManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<CouponManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/empik/couponManager/CouponManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.empik.couponManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CouponManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
